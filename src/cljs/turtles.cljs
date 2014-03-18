517b94b1046f0d8306a9162d38b3b999fb5bc96e
jarppe <jarppe@metosin.fi>
2014-03-18 20:41:03 +0200

    'smoother', as requested by Kaapo

--- src/cljs/turtles.cljs
+++ src/cljs/turtles.cljs
@@ -5,34 +5,52 @@
 
 (def app (atom nil))
 
-(defn set-display [id value]
-  (-> js/document
-      (.getElementById id)
-      (.-style)
-      (.-display)
-      (set! value)))
+(def deg180 Math/PI)
+(def deg90 (/ Math/PI 2.0))
+(def deg45 (/ Math/PI 4.0))
 
-(defn paint-turtle [ctx img {:keys [x y d]}]
+(defn rnd [lo hi] (+ lo (rand (- hi lo))))
+
+(defn rand-turtle [w h]
+  (let [d (rnd (- deg45) deg45)]
+    (assoc (condp = (rand-int 4)
+             0  {:x -44
+                 :y (rnd -44 (+ h 44))
+                 :d (+ d deg90)}
+             1  {:x (+ w 44)
+                 :y (rnd -44 (+ h 44))
+                 :d (- d deg90)}
+             2  {:x (rnd -44 (+ w 44))
+                 :y -44
+                 :d (+ d deg180)}
+             3  {:x (rnd -44 (+ w 44))
+                 :y (+ h 44)
+                 :d d})
+           :v (rnd 1 3))))
+
+(defn gonez? [w h {:keys [x y]}]
+  (not (and (< -44 x (+ w 44))
+            (< -44 y (+ h 44)))))
+
+(defn paint-turtle [ctx img {:keys [x y d] :as turtle}]
   (.save ctx)
   (.translate ctx x y)
   (.rotate ctx d)
   (.drawImage ctx img -42 -42)
-  (.restore ctx))
+  (.restore ctx)
+  turtle)
 
-(defn direction [d x y w h]
-  (if (and (< 0 x w) (< 0 y h))
-   d
-   (+ d (/ Math/PI -4.0))))
-
-(defn move-turtle [w h {:keys [x y d v] :as t}]
+(defn move-turtle [w h {:keys [x y d v] :as turtle}]
   (let [dx (Math/sin d)
-        dy (- (Math/cos d))
-        x  (+ x (* dx v))
-        y  (+ y (* dy v))]
-    (assoc t
-           :x x
-           :y y
-           :d (direction d x y w h))))
+        dy (- (Math/cos d))]
+    (assoc turtle
+           :x (+ x (* dx v))
+           :y (+ y (* dy v)))))
+
+(defn add-turtles [w h turtles]
+  (if (< (count turtles) 10)
+    (cons (rand-turtle w h) turtles)
+    turtles))
 
 (defn run []
   (let [{:keys [ctx img turtles]} @app
@@ -49,20 +67,19 @@
     (set! (.-font ctx) "18px sans-serif")
     (set! (.-fillStyle ctx) "rgb(32,255,32)")
     (.fillText ctx "Pepin kilpikonnat" (/ width 2) height)
-    (doseq [turtle turtles]
-      (paint-turtle ctx img turtle))
-    (swap! app assoc
-           :turtles (map (partial move-turtle width height) turtles))))
+    (swap! app assoc :turtles (->> turtles
+                                   (remove (partial gonez? width height))
+                                   (add-turtles width height)
+                                   (map (partial paint-turtle ctx img))
+                                   (map (partial move-turtle width height))
+                                   (doall)))))
 
-(defn rand-turtle [w h]
-  {:x (* (rand) w) 
-   :y (* (rand) h)
-   :d (* (rand) (* 2 Math/PI))
-   :v (* (rand) 3)})
-
-(defn rand-turtles [w h]
-  (cons (rand-turtle w h)
-        (lazy-seq (rand-turtles w h))))
+(defn set-display [id value]
+  (-> js/document
+      (.getElementById id)
+      (.-style)
+      (.-display)
+      (set! value)))
 
 (defn main []
   (let [canvas (.getElementById js/document "c")
@@ -75,7 +92,7 @@
     (reset! app {:canvas  canvas
                  :ctx     ctx
                  :img     img
-                 :turtles (take (+ 3 (rand-int 7)) (rand-turtles width height))}))
+                 :turtles []}))
   (set-display "loading" "none")
   (set-display "c" "table-cell")
   (.setInterval js/window run 16))
